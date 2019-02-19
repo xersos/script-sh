b964c22a94df757d85a1782c0ae0b476a42ba372
ToxiquOi <gau.vui97@hotmail.com>
2019-02-19 11:38:35 +0100

    Mon PenGin v2.0

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
-sudo su
-wget -bqc 
-https://www.apachefriends.org/xampp-files/7.3.2/xampp-linux-x64-7.3.2-0-installer.run
-apt -y install git
-chmod +x ./xampp
+sudo wget https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.2/xampp-linux-x64-7.3.2-0-installer.run?r=&ts=1550569831&use_mirror=freefr -O xampp.run
+sudo apt -y install git
+sudo chmod +x ./xampp.run
+sudo echo -e "Y Y \n Y" | ./xampp.run
