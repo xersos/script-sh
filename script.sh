d45bcc29533449c7ba6b313f29ed23eb449d0679
thomas.tosch <thomas.tosch@uha.fr>
2019-02-19 13:16:45 +0100

    ByTanCu.le

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-sudo wget -O xampp.run https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.2/xampp-linux-x64-7.3.2-0-installer.run?r=&ts=1550569831&use_mirror=freefr
+sudo wget -O xampp.run -q --show-progress "https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.2/xampp-linux-x64-7.3.2-0-installer.run?r=&ts=1550569831&use_mirror=freefr"
 sudo apt -y install git
 sudo chmod +x ./xampp.run
-sudo echo -e "Y Y \n Y" |sudo sh -r xampp.run
+sudo echo -e "Y Y \n Y" | sudo ./xampp.run --mode text
