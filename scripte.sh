06bd83731a3cab8c0181bc52a872bafa00d3b4a2
Sterioss <thomas.tosch@uha.fr>
2019-02-25 15:00:36 +0100

    Create scripte.sh

---
+++ scripte.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+sudo wget -O xampp.run -q --show-progress "https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.2/xampp-linux-x64-7.3.2-0-installer.run?r=&ts=1550569831&use_mirror=freefr"
+sudo apt -y install git
+sudo chmod +x ./xampp.run
+sudo echo -e "Y Y \n Y" | sudo ./xampp.run --mode text
+sudo ln -s $HOME/Bureau /opt/lampp/htdocs
+sudo touch /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo sh -c "echo '#!/bin/sh -e' >> /etc/rc.local"
+sudo sh -c "echo '/opt/lampp/lampp start' >> /etc/rc.local"
+sudo sh -c "echo 'exit 0' >> /etc/rc.local"
