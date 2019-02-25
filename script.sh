5af83ba10edf72048148b854bc7456834864e553
Sterioss <thomas.tosch@uha.fr>
2019-02-25 15:13:01 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 sudo wget -O xampp.run -q --show-progress "https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.2/xampp-linux-x64-7.3.2-0-installer.run?r=&ts=1550569831&use_mirror=freefr"
 sudo apt -y install git net-tools
 sudo chmod +x ./xampp.run
+sudo chmod 755 -R /opt/lampp/htdocs
 sudo echo -e "Y Y \n Y" | sudo ./xampp.run --mode text
 sudo ln -s /opt/lampp/htdocs $HOME/Bureau
 sudo touch /etc/rc.local
